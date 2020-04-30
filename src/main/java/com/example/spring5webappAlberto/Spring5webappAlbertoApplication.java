7322e46cdac6a3b07e4b9f7cb3516f45d59c221d
albyrea-git <Albertino#1>
2020-04-30 18:30:02 +0200

    commit impanto spring initialz

---
+++ src/main/java/com/example/spring5webappAlberto/Spring5webappAlbertoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring5webappAlberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappAlbertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappAlbertoApplication.class, args);
+	}
+
+}
